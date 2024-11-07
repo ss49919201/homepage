9f6beced9cc72d8f52dcc95ef337e88fd2bf8b42
ss49919201 <sakashin10291029@gmail.com>
2024-11-07 18:17:16 +0900

    feat: header and list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,50 +4,54 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h2 className="text-3xl sm:text-5xl font-bold text-center sm:text-left">
+          shinshin
+        </h2>
+        <p>Programmer💻</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <h3
+          className="text-xl sm:text-2xl font-semibold text-center sm:text-left"
+          style={{ color: "var(--geist-foreground)" }}
+        >
+          Skils
+        </h3>
+
+        <h3
+          className="text-xl sm:text-2xl font-semibold text-center sm:text-left"
+          style={{ color: "var(--geist-foreground)" }}
+        >
+          Links
+        </h3>
+        <li>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="hover:underline hover:underline-offset-4"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            X
           </a>
+        </li>
+        <li>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="hover:underline hover:underline-offset-4"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            GitHub
           </a>
-        </div>
+        </li>
+        <li>
+          <a
+            className="hover:underline hover:underline-offset-4"
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Blog
+          </a>
+        </li>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
