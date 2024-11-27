be45d16e87cf4a4e014e434847fb956d51c47ec1
ss49919201 <sakashin10291029@gmail.com>
2024-11-27 13:23:57 +0900

    feat: repace Next.js to vite + react

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# homepage
-This is homepage.
+# Homepage
--- src/components/ui/Footer/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <p>© 2024 shinshin</p>
-    </footer>
-  );
-}
